2cd51b342ebcd8b33ccb80270bd652b8e701a65f
NoellyCosta <noellysallazar@gmail.com>
2021-03-02 09:00:04 -0300

    add files

---
+++ semana4/funcoes/index.js
@@ -0,0 +1,29 @@
+// Exercício 1 -
+
+// a) vai ser impresso a variável* 5, no primeiro console.log 5*2, vai imorimir 10 e no segundo console.log vai imprimir 50 já que 5*10 é 50
+
+// b) não iria aparecer nada no console
+
+// Exercício 2-
+// a) As sídas vão ser os dois primeiros nomes da lista, pois o indice é menor que 2. Já que i++(significa index +1)
+
+// b) Ele iria imprimir (Amanda, Caio)
+
+// Exercício 3- 
+
+// a)  ele faz a variável receber um array, o arrayFinal está vazio pois irá receber os valores através do
+// .push. Em seguida ele percorre a array e vem com um (se o valor recebido for divisivel por 2 e sem resto). Depois ele multiplica a o resultado da divisao por ele mesmo e guarda no array
+
+// Exercício 4- 
+
+// a) 
+
+const name = 'Noelly'
+const age = 21
+const city = 'Arapiraca'
+const studentYON = 'sou'
+
+let imprimeDados = function(nome){
+console.log('Eu sou' + $(nome), 'tenho' + $(age), 'anos,' + ' moro em ' + $(city) + 'e' + $(studentYON)+ 'estudante.')
+}
+console.log('good night')
