95d81726d45408e9348354cfd335712736cb559b
NoellyCosta <noellysallazar@gmail.com>
2021-06-08 01:27:03 -0300

    funçao de pegar tarefas criadas e configuradas

---
+++ semana16.ToDoList/src/endpoints/getTaskById.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express"
+import moment from "moment"
+import selectTaskById from "../data/selectTaskById"
+
+export default async function getTaskById(
+    req: Request,
+    res: Response
+) {
+    try {
+        const result = await selectTaskById(req.params.id)
+
+        if (!result) {
+            res.status(404).send({
+                message: "Tarefa não encontrada"
+            })
+
+            return
+        }
+
+        res.status(200).send({
+            id: result.id,
+            description: result.description,
+            deadline: moment(result.deadline, "YYYY-MM-DD").format("DD-MM-YYYY"),
+            status: result.status,
+            authorId: result.author_id,
+            authorNIckname: result.nickname
+        })
+        
+    } catch (error) {
+        res.status(400).send({
+            message: error.message || error.sqlMessage
+        })
+    }
+}
