1e80c9096f311fa9540dacf93586bae3b357aa91
NoellyCosta <noellysallazar@gmail.com>
2021-07-21 18:17:04 -0300

    Exercicios do 1 ao 4

---
+++ semana15/APIS-REST/template-aula44/src/exercicios.ts
@@ -0,0 +1,30 @@
+//1
+
+// a) Methodo get
+
+//b) ("/users")
+
+// 2)
+
+//a) Por    query params
+
+//b) Fazendo um tipo de validação
+
+//3) 
+
+//a) Ele recebe um input como parametro
+
+//b) if(!user.name){
+ //   return "Usuário não encontrado"
+//}else{
+ //   return ("Sucess")
+//}
+
+//4)) 
+
+//a)  com o metodo PUT é possível fazer alterações
+
+//b) Ele tem um escopo mais definido e permite fazer alterações nos dados
+
+
+
