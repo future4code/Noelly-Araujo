7968d22629760109ff570c9003ec60e2abea1855
NoellyCosta <noellysallazar@gmail.com>
2021-10-09 22:28:54 -0300

    letras C e D do exercicio 4

--- semana20/testes-automatizados/tests/testVerifyAge.test.ts
+++ semana20/testes-automatizados/tests/testVerifyAge.test.ts
@@ -1,5 +1,4 @@
 import { verifyAge } from "../src/business/verifyAge";
-import { Result } from "../src/models/userCasino";
 import { Casino, LOCATION, User } from "../src/models/userCasino";
 import { NATIONALITY } from "../src/models/userCasino";
 
@@ -7,19 +6,97 @@ describe("Teting as function verifyAge", () => {
 
 
     test("Brazilian passed", () => {
-        
-       const brazilian: User = {
-           name: "Josisklaiton",
-           age: 18,
-           nationality: NATIONALITY.BRAZILIAN
-       }
 
-       const casino: Casino = {
-           name: "Storm",
-           location: LOCATION.BRA
-       }
+        const brazilian: User = {
+            name: "Josisklaiton",
+            age: 18,
+            nationality: NATIONALITY.BRAZILIAN
+        }
 
-       const result: Result = verifyAge(casino, [brazilian])
-       expect(result.brazilians.allowed).toEqual(["Josisklaiton"])
+        const casino: Casino = {
+            name: "Storm",
+            location: LOCATION.BRA
+        }
+
+        const result = verifyAge(casino, [brazilian])
+        expect(result.brazilians.allowed).toEqual(["Josisklaiton"])
+    })
+
+    test("American people in casino of Brazil", () => {
+        const brazilian: User = {
+            name: "Thomas",
+            age: 18,
+            nationality: NATIONALITY.AMERICAN
+        }
+
+        const casino: Casino = {
+            name: "reborn",
+            location: LOCATION.BRA
+        }
+
+        const result = verifyAge(casino, [brazilian])
+        expect(result.americans.allowed).toEqual(["Thomas"])
+
+    })
+
+    test("Four users from Brazil and US not passed in casino in the US", () => {
+        const brazilian: User = {
+            name: "userBR",
+            age: 19,
+            nationality: NATIONALITY.BRAZILIAN
+        }
+
+        const american: User = {
+            name: "userUS",
+            age: 19,
+            nationality: NATIONALITY.AMERICAN
+        }
+
+        const casino: Casino = {
+            name: "Olulu",
+            location: LOCATION.EUA
+        }
+
+        const result = verifyAge(casino,
+            [
+                brazilian,
+                brazilian,
+                american,
+                american,
+            ]);
+
+        expect(result.brazilians.unallowed).toEqual(["userBR", "userBR"])
+        expect(result.americans.unallowed).toEqual(["userUS", "userUS"])
+    })
+
+    test("passing american users in Casino US", () => {
+        const brazilian: User = {
+            name: "userBR",
+            age: 19,
+            nationality: NATIONALITY.BRAZILIAN
+        }
+
+        const american: User = {
+            name: "userUS",
+            age: 21,
+            nationality: NATIONALITY.AMERICAN
+        }
+
+        const casino: Casino = {
+            name: "Olulu",
+            location: LOCATION.EUA
+        }
+
+        const result = verifyAge(casino,
+            [
+                brazilian,
+                brazilian,
+                american,
+                american,
+            ]);
+
+        expect(result.brazilians.unallowed).toEqual(["userBR", "userBR"])
+        expect(result.americans.allowed).toEqual(["userUS", "userUS"])
     })
 })
+
