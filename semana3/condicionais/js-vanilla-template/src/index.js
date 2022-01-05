3be9f9ecdd6c0b1097cf1a7b53cafa437884dd0e
NoellyCosta <noellysallazar@gmail.com>
2022-01-05 17:26:20 -0300

    exercicios adicionados, + desafios 1 e 2

--- semana3/condicionais/js-vanilla-template/src/index.js
+++ semana3/condicionais/js-vanilla-template/src/index.js
@@ -74,11 +74,116 @@ let precoFilme = Number(prompt('Qual é o valor do ingresso'))
 
 let snack = prompt('Qual snack deseja comprar?')
 
-if( filmes === 'fantasia' && precoFilme < 15){
-   console.log('Bom filme!', 'com',snack)
-}else{
+if (filmes === 'fantasia' && precoFilme < 15) {
+    console.log('Bom filme!', 'com', snack)
+} else {
     console.log('Escolha outro filme.')
 }
 
-/// cont. desafio...
+// Exercícios aula 13
 
+
+// Exercício 01 
+
+// ele vai contar executar o código até dar 5
+
+// Exercicio 02
+
+// a) Ele vai passar pelos números maiores que 18
+
+//b) Sim, só o for of já seria o suficiente. 
+
+//const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+//for (let numero of lista) {
+//  
+//        console.log(numero)
+//    
+//}
+
+// Exercício 3
+
+//a)
+const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+
+
+for (let numero of array) {
+    console.log(numero)
+}
+
+//b) 
+
+for (let numero of array) {
+    console.log(numero / 10)
+}
+
+//c) 
+
+let novoArray = []
+
+for (let numero of array) {
+    if (numero % 2 === 0) {
+        novoArray.push(numero);
+    }
+}
+
+console.log(novoArray)
+
+
+//d)
+
+for (let i = 0; i < array.length; i++) {
+    console.log("O elemento do  índex é:", i, "é", array[i])
+}
+
+
+//e) 
+let valorMaximo = array[0]
+
+let valorMinimo = array[0]
+
+
+for (let i = 1; i < array.length; i++) {
+    if (valorMaximo < array[i]) {
+        valorMaximo = array[i]
+    } else if (valorMinimo > array[i]) {
+        valorMinimo = array[i]
+    }
+}
+
+console.log("O maior número é:", valorMaximo, "e o menor número é:", valorMinimo)
+
+//Desafios 
+
+//Desafio 1
+
+let numberPlayer1 = Number(prompt("Escolha um número"))
+
+while (isNaN(numero)) {
+    numero = Number(prompt("Escolha um número"))
+}
+
+let chute
+
+for (let tentativas = 1; numero != chute; tentativas++) {
+    chute = Number(prompt("Tente adivinhar:"))
+    console.log("O número chutado foi", chute)
+
+    if (numero === chute) {
+        console.log("Acertou!")
+        if (tentativas === 1) {
+            console.log("Você acertou de primeira, parabéns!!")
+        } else {
+            console.log("O número de tentativas foi", tentativas)
+        }
+    } else if (numero > chute) {
+        console.log("Xii muito longe, é muito maior")
+    } else if (numero > chute) {
+        console.log("Muito abaixo, é maior")
+    } else {
+        console.log("Errou, tente adicionar outro número")
+    }
+}
+
+// Desafio 2
+
+// foi regular e eu poderia ter diminuido a quantidade de ifs
